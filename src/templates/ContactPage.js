b1c9c2eca3ad3ce377c0f4c20cc84aa03533faf1
Devon Navon <devon.navon@gmail.com>
2021-05-17 00:01:42 -0400

    more

--- src/templates/ContactPage.js
+++ src/templates/ContactPage.js
@@ -5,7 +5,7 @@ import { graphql } from 'gatsby'
 import PageHeader from '../components/PageHeader'
 import FormSimpleAjax from '../components/FormSimpleAjax'
 import Content from '../components/Content'
-import GoogleMap from '../components/GoogleMap'
+// import GoogleMap from '../components/GoogleMap's
 import Layout from '../components/Layout'
 import './ContactPage.css'
 
